7970f00900a2ee8710928657c0f9550cbbf29616
Oylsister <kkuiba2009@gmail.com>
2025-11-05 22:54:57 +0700

    CanUse to CanEquip

--- ZombieModSharp/Core/Hooks.cs
+++ ZombieModSharp/Core/Hooks.cs
@@ -13,30 +13,37 @@ public class Hooks : IHooks
     private readonly ISharedSystem _sharedSystem;
     private readonly IPlayer _player;
     private readonly IHookManager _hookManager;
+    private readonly IModSharp _modsharp;
 
     public Hooks(ISharedSystem sharedSystem, IPlayer player)
     {
         _sharedSystem = sharedSystem;
         _player = player;
         _hookManager = _sharedSystem.GetHookManager();
+        _modsharp = _sharedSystem.GetModSharp();
     }
 
     public void PostInit()
     {
-        _hookManager.PlayerWeaponCanUse.InstallHookPre(OnCanUse);
+        _hookManager.PlayerWeaponCanEquip.InstallHookPre(OnCanEquip);
     }
 
-    private HookReturnValue<bool> OnCanUse(IPlayerWeaponCanUseHookParams param, HookReturnValue<bool> result)
+    private HookReturnValue<bool> OnCanEquip(IPlayerWeaponCanEquipHookParams param, HookReturnValue<bool> result)
     {
         var player = param.Client;
         var weapon = param.Weapon;
-        
+
+        //var isInfected = _player.IsClientInfected(player);
+        //_modsharp.PrintChannelAll(HudPrintChannel.Chat, $"Zombie Status: {isInfected}");
+
         // if player is infect and weapon is knife then ignore all of it.
-        if(_player.IsClientInfected(player) && !weapon.Name.Contains("knife", StringComparison.OrdinalIgnoreCase))
+        if (_player.IsClientInfected(player) && !weapon.IsKnife)
         {
-            return new HookReturnValue<bool>(EHookAction.SkipCallReturnOverride);
+            //_modsharp.PrintChannelAll(HudPrintChannel.Chat, $"This is {EHookAction.SkipCallReturnOverride} and {result.ReturnValue}");
+            return new HookReturnValue<bool>(EHookAction.SkipCallReturnOverride, false);
         }
 
+        //_modsharp.PrintChannelAll(HudPrintChannel.Chat, $"This is {result.Action} and {result.ReturnValue}");
         return result;
     }
 }
